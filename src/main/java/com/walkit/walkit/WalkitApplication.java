ac9df1600326bcfd9642f285807f51b2b82c3ff2
Teach-D <wkadht0619@gmail.com>
2025-12-10 21:05:03 +0900

    first commit

---
+++ src/main/java/com/walkit/walkit/WalkitApplication.java
@@ -0,0 +1,13 @@
+package com.walkit.walkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkitApplication.class, args);
+	}
+
+}
