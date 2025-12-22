4c1839c718b1bb7623cfdbf07e33a843f3e45c7a
unknown <wkadht0619@gmail.com>
2025-12-22 17:16:18 +0900

    add: 캐릭터별 아이템 착용 이미지 #65

---
+++ src/main/java/com/walkit/walkit/common/image/repository/CharacterWearImageRepository.java
@@ -0,0 +1,15 @@
+package com.walkit.walkit.common.image.repository;
+
+import com.walkit.walkit.common.image.entity.CharacterWearImage;
+import com.walkit.walkit.domain.character.enums.Grade;
+import com.walkit.walkit.domain.character.enums.ItemName;
+import com.walkit.walkit.domain.character.enums.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CharacterWearImageRepository extends JpaRepository<CharacterWearImage, Long> {
+    List<CharacterWearImage> findByPositionAndGrade(Position position, Grade grade);
+
+    CharacterWearImage findByPositionAndGradeAndItemName(Position position, Grade grade, ItemName itemName);
+}
